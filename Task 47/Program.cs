f8ec59001184e5b9e430deba9742c5fa54a3e874
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-29 21:44:49 +0300

    Add Task 47

---
+++ Task 47/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 47: Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{ 
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        { 
+            Console.Write($"{array[i, j]} ");
+        }
+            Console.WriteLine();
+    }
+}
+
+double[,] myArray = GetArray(m, n, 0, 100);
+PrintArray(myArray);
