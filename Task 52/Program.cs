a5348263bf356852a29156a6cbb41e8cd9129db4
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-29 21:50:24 +0300

    Add Task 52

---
+++ Task 52/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.*/
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] AverageColumn(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / rows;
+        }
+    }
+    return result;
+}
+
+void PrintNewArray(double[] array)
+{
+   Console.WriteLine("Средние значения по столбцам:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]:f1}  ");
+    }
+}
+
+int[,] arrayResult = GetArray(rows, columns);
+PrintArray(arrayResult);
+Console.WriteLine();
+double[] averageclmn = AverageColumn(arrayResult);
+PrintNewArray(averageclmn);
